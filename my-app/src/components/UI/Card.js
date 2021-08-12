73e254decb63f2147cc37b88735ac1b3640a22ad
DESKTOP-0PG3ALK\kienh <kienhieu98@gmail.com>
2021-08-12 19:42:07 +0700

    innit project

---
+++ my-app/src/components/UI/Card.js
@@ -0,0 +1,10 @@
+import './Card.css'
+const Card = (props) => {
+    const cardClass = 'card ' + props.className;
+    return (
+        <div className={cardClass}>
+            {props.children}
+        </div>
+    );
+}
+export default Card;
